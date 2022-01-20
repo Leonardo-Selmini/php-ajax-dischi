acbd174efeede99d983fd2659d94109be3c9644c
Leonardo.Selmini <selminileonardo@gmail.com>
2022-01-20 16:16:42 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+      songs: []
+  },
+  // created() {
+  //   axios.get("http://localhost/php-ajax-dischi/backEnd.php")
+  //   .then((response) => {
+  //     this.songs = response.data;
+  //   });
+  // }
+});
